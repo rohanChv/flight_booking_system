51c4a3f88b8586587506d4916289dade47c3b17f
analyst <rohangchavan@gmail.com>
2021-12-01 07:34:57 +0530

    added fare

--- fare/src/main/java/com/example/demo/controller.java
+++ fare/src/main/java/com/example/demo/controller.java
@@ -1,13 +1,34 @@
 package com.example.demo;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fare")
 public class controller {
-	@RequestMapping()
-	public String hey() {
-		return "hey";
+@Autowired
+repo rp;
+
+@GetMapping("/getfare/{id}")
+public Optional<fare> getfare(@PathVariable String id) {
+		return rp.getinfo(id);
+}
+@PostMapping("/getfare/add")
+public String add(@RequestBody fare f1) {
+	try {
+		rp.insert(f1);
+		return "sucess";
+	}catch(Exception e) {
+		return "sorry problem in creation ";
 	}
+	
+}
 }
 
