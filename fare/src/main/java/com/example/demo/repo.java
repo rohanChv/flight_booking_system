d41a50a9dc3933617c3e4b7af57f09e0cdf67006
analyst <rohangchavan@gmail.com>
2021-12-01 07:35:15 +0530

    added fare

---
+++ fare/src/main/java/com/example/demo/repo.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface repo extends MongoRepository<fare,String>{
+	@Query("{'fid':?0}")
+	public Optional<fare>getinfo(String fid);
+
+}
