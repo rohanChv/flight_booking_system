4508ee88886fd054fd16a33104f58e37f494485c
analyst <rohangchavan@gmail.com>
2021-12-03 19:37:32 +0530

    n

--- search/src/main/java/com/example/demo/SearchApplication.java
+++ search/src/main/java/com/example/demo/SearchApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -11,5 +12,4 @@ public class SearchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SearchApplication.class, args);
 	}
-
 }
